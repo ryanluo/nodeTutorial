43c62fcd2a84e5ed99256a9a7db606898585f299
usuallyacorn <rllluo@gmail.com>
2013-10-16 16:59:57 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+var express = require('express');
+
+var app = express();
+
+// Configuration
+app.configure( function() {
+});
+
+// Routes
+app.get('/', function(req, res) {
+    res.send('Hello World');
+});
+
+app.listen(3000);
